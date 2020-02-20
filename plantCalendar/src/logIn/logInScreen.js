bbc820eaae7f01e4a3f5169a85db2158e03e2337
Yuki Wang <yukiluna1999@gmail.com>
2020-02-20 11:23:24 -0800

    feat(loginScreen): add logo, edit login button
    refractor(gitignore): added extra gitignore file to ignore DS_Store

--- plantCalendar/src/logIn/logInScreen.js
+++ plantCalendar/src/logIn/logInScreen.js
@@ -1,15 +1,51 @@
 import * as React from 'react';
-import {View, Text, Button} from 'react-native';
+// TODO: remove Alert when we don't need it anymore
+import { StyleSheet, View, Text, Button, Image, Alert} from 'react-native';
 
-export default class logInScreen extends React.Component {
+/**
+ * logInScreen Class
+ * TODO: We might not need a class if we are generating the view?
+ *       Do we need any other feature (member function)?
+ * 
+ * \brief Generate the view for login screen
+ */
+export default class logInScreen extends React.Component { 
+    /**
+     * TODO: Discuss placement of this function
+     *  Do we want to put this function as a member function? 
+     *  Or we can put this function in a separate file and export it
+     */
+    logInWithGoogle ()  {
+        // TODO: remove this when we actually implement login with google
+        // alert the user that this button is clicked
+        // App seems laggy when alert and navigate tries to run at the same time
+        Alert.alert("Continue to Google button clicked!");
+
+        this.props.navigation.navigate('Home');
+    };
+
     render () {
         return (
-            <View>
+            <View style={styles.container}>
                 <Text>Login Screen</Text>
+                <Image
+                    style={styles.logo}
+                    source={require('../../assets/loginLogo.png')}/>
                 <Button
-                    title="Go to Home"
-                    onPress={() => this.props.navigation.navigate('Home')}/>
+                    title="Continue With Google"
+                    onPress={() => this.logInWithGoogle()}/>
             </View>
         );
     };
 }
+
+const styles = StyleSheet.create({
+    container:{
+        // control how the children align horizontally
+        alignItems: 'center',
+    },
+    logo: {
+        width: 150,
+        height: 150,
+    },
+});
