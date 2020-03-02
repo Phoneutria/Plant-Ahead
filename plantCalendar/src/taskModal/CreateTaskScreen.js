6c122f1f677b432e1355ea0f8c37a8548610fb6c
Jkwok2018 <jessica.kwok19@gmail.com>
2020-03-02 00:03:45 -0800

    docs(CreateTaskScreen) Added comments and TODOs

--- plantCalendar/src/taskModal/CreateTaskScreen.js
+++ plantCalendar/src/taskModal/CreateTaskScreen.js
@@ -12,6 +12,8 @@ export default class CreateTaskScreen extends React.Component {
     priority: "medium",
     hoursLeft: 0 
   }
+  // TODO: instead of this function, it would be a function that
+  // create new task and returns back to the home page
   // Tempporary function to check if text input and date picker worked
   formatOutput() {
     let temp = String(this.state.dueDate).split(' ');
@@ -34,14 +36,7 @@ export default class CreateTaskScreen extends React.Component {
       this.setState({dueDate: selectedDate});
     };
 
-    const onChangeName = (event, updateName) => {
-      this.setState({name: updateName});
-    }
-
-    const onChangeHours = (event, updatehours) => {
-      this.setState({hoursLeft: updatehours});
-    }
-
+    // options for priority
     let data = [
       {value: 'high'},
       {value: 'medium'},
@@ -50,41 +45,45 @@ export default class CreateTaskScreen extends React.Component {
 
   return (
     <View>
-        <TextInput
-                style={styles.input}
-                onChangeText={(text)=>{
-                  this.setState({name:text});
-                }}
-                placeholder="name"
-            />
+      {/* Entering name of the task */}
+      <TextInput
+              style={styles.input}
+              onChangeText={(text)=>{
+                this.setState({name:text});
+              }}
+              placeholder="name"
+          />
+      {/* Entering number of hours needed for the task */}
+      <TextInput
+          style={styles.input}
+          onChangeText={(text)=>{
+            this.setState({hoursLeft:text});
+          }}
+          // TODO: make sure the input is a number
+          placeholder="Estimate hours needed"
+      />
+      {/* For selecting the due date */}
+      <Text>Due Date</Text>
+      <DateTimePicker 
+        mode={'date'}
+        value={ dueDate }
+        onChange={onChangeDate} 
+      />
 
-            <TextInput
-                style={styles.input}
-                onChangeText={(text)=>{
-                  this.setState({hoursLeft:text});
-                }}
-                placeholder="Estimate hours needed"
-            />
+      {/* For selecting the priority */}
+      <Dropdown
+          label='Priority'
+          data={data}
+          onChangeText={(value)=>{
+            this.setState({priority:value});
+          }}
+      />
 
-            <Text>Due Date</Text>
-            {/* For selecting the due date */}
-            <DateTimePicker 
-              mode={'date'}
-              value={ dueDate }
-              onChange={onChangeDate} 
-            />
-            <Dropdown
-                label='Priority'
-                data={data}
-                onChangeText={(value)=>{
-                  this.setState({priority:value});
-                }}
-            />
-
-            <Button
-                onPress={()=> Alert.alert(this.formatOutput())}
-                title='Submit'/> 
-                {/* this.formatOutput() */}
+      {/* For creating the task */}
+      <Button
+          onPress={()=> Alert.alert(this.formatOutput())}
+          title='Submit'/> 
+          {/* this.formatOutput() */}
     </View>
   );
   }
