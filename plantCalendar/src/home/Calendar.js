454d612abc06aa76c434ebc98a8f43a0d41c5402
Kariessa <kariessa.schultz@gmail.com>
2020-03-16 16:54:36 -0700

    feat(Calendar.js): function to update user's Google Tasks

--- plantCalendar/src/home/Calendar.js
+++ plantCalendar/src/home/Calendar.js
@@ -165,6 +165,34 @@ export default class Calendar extends React.Component {
     }
 
     /**
+     * \brief modifies task data in user's Google Tasks
+     * \detail
+     *      updates task's name (taskName) and due date (dueDate) in user's Google Tasks
+     *      can also tell Google Tasks that the task is complete
+     *      taskName is a string, taskListId is a string, dueDate is a string, 
+     *      completion is a bool (true for completed)
+     *      taskId identifies the task
+     */
+    updateGoogleTask = async (taskId, taskListId, taskName, dueDate, completion) => {
+        // get task from user's Google Tasks
+        let task = await fetch('https://www.googleapis.com/tasks/v1/' + taskListId +
+        "/tasks/" + taskId, {
+            headers: { Authorization: `Bearer ${this.props.accessToken}`},
+        }).catch(error => console.log("error message: " + error));
+        
+        // update task properties
+        task.title = taskName;
+        task.due = dueDate;
+        task.completed = completion;
+        
+        // send modified task back to user's Google Tasks
+        var xhr = new XMLHttpRequest();
+        xhr.open("PUT", "https://www.googleapis.com/tasks/v1/lists/" + taskListId + "/tasks/" + taskId,
+        true);
+        xhr.send(task);
+    }
+
+    /**
      * \brief render each task based on its information
      * \detail
      *      The parent (Calendar)'s deleteCompletedTask function gets passed down to
