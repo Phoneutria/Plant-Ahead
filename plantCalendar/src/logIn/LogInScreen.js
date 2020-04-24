41c927690fc118611ea8ef8fc81941588d62ac3c
Jkwok2018 <jessica.kwok19@gmail.com>
2020-04-24 13:58:17 -0700

    feat (LoginScreen): added a splash screen that waits for the resources to load before rendering

--- plantCalendar/src/logIn/LogInScreen.js
+++ plantCalendar/src/logIn/LogInScreen.js
@@ -11,7 +11,7 @@ import FirestoreHandle from '../dataHandlers/FirestoreHandle';
 
 import {iosClientId} from '../../credentials/iosClientId';
 import {androidClientId} from '../../credentials/androidClientId';
-import { LinearGradient } from 'expo-linear-gradient';
+import { AppLoading } from 'expo';
 
 /**
  * LogInScreen Class
@@ -25,6 +25,7 @@ export default class LogInScreen extends React.Component {
         userEmail: "",
         userName: "",
         firestoreHandle: new FirestoreHandle(),
+        fontsLoaded: false,
     }
     
     /**
@@ -44,8 +45,9 @@ export default class LogInScreen extends React.Component {
                 'https://www.googleapis.com/auth/calendar.events',
                 'https://www.googleapis.com/auth/tasks',
                 ],
+                //for loading screen purposes, reference _cacheResourcesAsync() desicription below
+                isReady: false,
             });
-
             if (result.type === 'success') {
                 this.setState({userEmail: result.user.email, userName: result.user.name});
                 this.firebaseSignUpSignIn(result.accessToken);
@@ -105,38 +107,48 @@ export default class LogInScreen extends React.Component {
         }).catch(error => console.log(error));
     }
 
+    /**
+     * \brief: returns a promise after the image is loaded so that the Login screen can replace
+     * the loading screen
+     */
+    async _cacheResourcesAsync() {
+        const images = [require('../../assets/Picture1.png')];
+    
+        const cacheImages = images.map(image => {
+          return Asset.fromModule(image).downloadAsync();
+        }); 
+        return Promise.all(cacheImages);
+    }
+
     render () {  
+        // Tells the loading screen to appear until _cacheResourcesAsync returns a promise
+        // updates the state variable isReady to true
+        if (!this.state.isReady) {
+            return (
+              <AppLoading
+                startAsync={this._cacheResourcesAsync}
+                onFinish={() => this.setState({ isReady: true })}
+                onError={console.warn}
+              />
+            );
+        }
+            
         return (
             <View style={styles.container}>
-                 <LinearGradient
-                // white '#ffffff'
-                // light green '#b0e099'
-                // green '#8ccd82'
-                // brown '#c06318'
-                // redish pink '#e37957'
-                // brain pink  '#dc9b9b'
-                // yellow #fff8d4'
-                    colors={['#ffffff','#72a669']}
-                    style={{
-                        position: 'absolute',
-                        left: 0,
-                        right: 0,
-                        top: 0,
-                        height: 900,
-                    }}
-                    />
+                <Text style={styles.headingText}>Welcome To Plant Ahead!</Text>
                 <Image
                     style={styles.logo}
                     source={require('../../assets/Picture1.png')}/>
                 <TouchableOpacity
                 style={styles.button}
                 onPress={() => this.signInWithGoogleAsync()}>
-                    <Text style={styles.text}> Login With Google</Text>
+                    <Text style={styles.buttonText}> Login With Google</Text>
                 </TouchableOpacity>
             </View>
         );
-    };
+    }  
 }
+
 
 const styles = StyleSheet.create({
     container:{
@@ -146,29 +158,42 @@ const styles = StyleSheet.create({
         flexDirection:'column',
         alignItems:'center',
         justifyContent:'center',
-        backgroundColor: '#72a669'//'#fdd8c0'
+        backgroundColor: '#ffffff'//'#fdd8c0'
     },
     logo: {
         width: 200,
         height: 200,
-        marginLeft:30,
+        marginLeft:20,
+        marginTop:20,
     },
     button: {
         marginTop:30,
         marginBottom:100,
         paddingTop:15,
         paddingBottom:15,
-        marginLeft:40,
+        marginLeft:30,
         marginRight:30,
         borderRadius:10,
         padding: 20,
-        backgroundColor: '#8ccd82'
+        backgroundColor: '#eebaba'
     },
-    text: {
+    buttonText: {
         color: '#ffffff',
-        fontFamily: "Cochin",
-        fontSize: 25,
+        fontSize: 20,
         fontWeight: "bold"
+    },
+    headingText:{
+        color: '#8ccd82',
+        fontSize: 30,
+        fontWeight: "bold",
     }
 });
 
+// TODO: these are the colors that might be used, delete later
+// white '#ffffff'
+// light green '#b0e099'
+// green '#8ccd82'
+// brown '#c06318'
+// redish pink '#e37957'
+// brain pink  '#dc9b9b'
+// yellow #fff8d4'
